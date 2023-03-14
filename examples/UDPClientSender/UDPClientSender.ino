c4e55cd3a0bf9c405e45e8ec012d342926800525
lewishe <lewishe@outlook.com>
2023-03-14 14:37:08 +0800

    Correct incorrect pin assignments

--- examples/UDPClientSender/UDPClientSender.ino
+++ examples/UDPClientSender/UDPClientSender.ino
@@ -20,7 +20,7 @@
 #define ETH_CLK_MODE ETH_CLOCK_GPIO17_OUT
 
 // Pin# of the enable signal for the external crystal oscillator (-1 to disable for internal APLL source)
-#define ETH_POWER_PIN 16
+#define ETH_POWER_PIN -1
 
 // Type of the Ethernet PHY (LAN8720 or TLK110)
 #define ETH_TYPE ETH_PHY_LAN8720
