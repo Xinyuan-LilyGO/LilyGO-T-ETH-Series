1c5b181bce1034f0c4d39af074d233217154974b
lewisxhe <lewishe@outlook.com>
2024-04-13 20:05:21 +0800

    Fix compilation errors for lower versions (2.x)

--- examples/InternetComTest/InternetComTest.ino
+++ examples/InternetComTest/InternetComTest.ino
@@ -11,6 +11,7 @@
 
 #if ESP_ARDUINO_VERSION < ESP_ARDUINO_VERSION_VAL(3,0,0)
 #include <ETHClass2.h>       //Is to use the modified ETHClass
+#define ETH  ETH2
 #include <WiFiClientSecure.h>
 WiFiClientSecure clientSecure;
 #else
